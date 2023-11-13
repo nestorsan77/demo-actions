0425f776817c4b8a47e8aa78f88cee5f060fab35
nestorsan77 <nestorsan77@gmail.com>
2023-11-13 16:47:52 +0100

    pruebas unitarias calculadora hechas

--- suma.py
+++ suma.py
@@ -1,5 +1,16 @@
 def sumar(a,b):
     return a+b
 
+def restar(a,b):
+    return a-b
+
+def multiplicar(a,b):
+    return a*b
+
+def dividir(a,b):
+    if(a==0 and b==0):
+        return 0;     
+    return a/b
+
 if __name__ == "__main__":
-    print(sumar(5,3))
+    print(restar(-1,1))
--- test_suma.py
+++ test_suma.py
@@ -1,5 +1,11 @@
-import unittest
+import unittest 
 from suma import sumar
+import unittest 
+from suma import restar
+import unittest 
+from suma import multiplicar
+import unittest 
+from suma import dividir
 
 class TestSumar(unittest.TestCase):
 
@@ -7,6 +13,15 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(3,2,), 5)
         self.assertEqual(sumar(-1,1,), 0)
         self.assertEqual(sumar(-1,-1,), -2)
+        self.assertEqual(restar(3,2,), 1)
+        self.assertEqual(restar(-1,1,), -2)
+        self.assertEqual(restar(-1,-1,), 0)
+        self.assertEqual(multiplicar(2,5,), 10)
+        self.assertEqual(multiplicar(2,-5,), -10)
+        self.assertEqual(multiplicar(0,0,), 0)
+        self.assertEqual(dividir(4,2,), 2)
+        self.assertEqual(dividir(0,0,), 0)
+        self.assertEqual(dividir(-5,-5,), 1)
 
 if __name__ == '__main__':
     unittest.main()
